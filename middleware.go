30e7147ff93d380ab8efb40c3b5bacbc205159c9
Masahiro Furudate <178inaba.git@gmail.com>
2020-07-14 03:06:11 +0900

    Fix logger middleware

--- middleware.go
+++ middleware.go
@@ -2,7 +2,6 @@ package logging
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
 	"cloud.google.com/go/logging"
@@ -23,7 +22,12 @@ func (l *contextLogger) Logger() echo.Logger {
 
 // LoggerMiddleware is appengine echo logger middleware.
 type LoggerMiddleware struct {
-	client *logging.Client
+	requestLogger     *logging.Logger
+	applicationLogger *logging.Logger
+
+	logLevel log.Lvl
+
+	httpFormat *propagation.HTTPFormat
 
 	moduleID  string
 	projectID string
@@ -32,56 +36,66 @@ type LoggerMiddleware struct {
 }
 
 // NewLoggerMiddleware returns appengine echo logger middleware.
-func NewLoggerMiddleware(client *logging.Client, moduleID, projectID, versionID, zone string) *LoggerMiddleware {
-	return &LoggerMiddleware{client: client, moduleID: moduleID, projectID: projectID, versionID: versionID, zone: zone}
+func NewLoggerMiddleware(client *logging.Client, logLevel log.Lvl, moduleID, projectID, versionID, zone string) *LoggerMiddleware {
+	opt := logging.CommonResource(&mrpb.MonitoredResource{
+		Type: "gae_app",
+		Labels: map[string]string{
+			"module_id":  moduleID,
+			"project_id": projectID,
+			"version_id": versionID,
+			"zone":       zone,
+		},
+	})
+	reqLogger := client.Logger(fmt.Sprintf("%s_request", moduleID), opt)
+	appLogger := client.Logger(fmt.Sprintf("%s_application", moduleID), opt)
+	return &LoggerMiddleware{
+		requestLogger:     reqLogger,
+		applicationLogger: appLogger,
+		logLevel:          logLevel,
+		httpFormat:        &propagation.HTTPFormat{},
+		moduleID:          moduleID,
+		projectID:         projectID,
+		versionID:         versionID,
+		zone:              zone,
+	}
 }
 
 // Logger is appengine echo logger middleware.
 // Set application logger to echo.Context and write request log.
 func (m *LoggerMiddleware) Logger(next echo.HandlerFunc) echo.HandlerFunc {
-	hf := &propagation.HTTPFormat{}
-
-	opt := logging.CommonResource(&mrpb.MonitoredResource{
-		Type: "gae_app",
-		Labels: map[string]string{
-			"module_id":  m.moduleID,
-			"project_id": m.projectID,
-			"version_id": m.versionID,
-			"zone":       m.zone,
-		},
-	})
-	reqLogger := m.client.Logger(fmt.Sprintf("%s_request", m.moduleID), opt)
-	appLogger := m.client.Logger(fmt.Sprintf("%s_application", m.moduleID), opt)
 	return func(c echo.Context) error {
 		req := c.Request()
-		sc, _ := hf.SpanContextFromRequest(req)
-		trace := fmt.Sprintf("projects/%s/traces/%s", m.projectID, sc.TraceID)
-		spanID := sc.SpanID.String()
 
-		logger := New(appLogger, trace, spanID)
-		logger.SetLevel(log.INFO)
+		var trace, spanID string
+		sc, ok := m.httpFormat.SpanContextFromRequest(req)
+		if ok {
+			trace = fmt.Sprintf("projects/%s/traces/%s", m.projectID, sc.TraceID)
+			spanID = sc.SpanID.String()
+		}
+
+		appLogger := New(m.applicationLogger, trace, spanID)
+		appLogger.SetLevel(m.logLevel)
 
 		start := time.Now()
-		if err := next(&contextLogger{Context: c, logger: logger}); err != nil {
+		if err := next(&contextLogger{Context: c, logger: appLogger}); err != nil {
 			c.Error(err)
 		}
 		end := time.Now()
 
 		resp := c.Response()
-		remoteIP := strings.Split(req.Header.Get("X-Forwarded-For"), ",")[0]
-		reqLogger.Log(logging.Entry{
+		m.requestLogger.Log(logging.Entry{
 			Timestamp: time.Now(),
-			Severity:  logger.maxSeverity,
+			Severity:  appLogger.maxSeverity,
 			HTTPRequest: &logging.HTTPRequest{
 				Request:      req,
-				Latency:      end.Sub(start),
 				Status:       resp.Status,
-				RemoteIP:     remoteIP,
 				ResponseSize: resp.Size,
+				Latency:      end.Sub(start),
+				RemoteIP:     c.RealIP(),
 			},
 			Trace:        trace,
-			TraceSampled: true,
 			SpanID:       spanID,
+			TraceSampled: true,
 		})
 
 		return nil
